c41dfde5a491d8f8f9d190536fb44d4e1d1cac51
Valiantsin <lutchenkovalentin@gmail.com>
2022-07-31 10:06:53 +0200

    rebuild the firstTest.js with another assertions

--- test/firstTest.js
+++ test/firstTest.js
@@ -1,9 +1,9 @@
 const {Builder, By, Key, WebDriver} = require("selenium-webdriver");
 require("chromedriver");
 let assert = require("assert");
-// const { IncomingMessage } = require("http");
-let should = require("chai").should();
-const { user, pass } = require('../credentials.js')
+var expect = require('expect')
+const { user, pass } = require('../utils/credentials.js')
+const { url, emaillName, pageTitle, headers} = require('../utils/constants.js')
 
 describe("smoke test for mail.ru", async function(){
     
@@ -11,12 +11,12 @@ describe("smoke test for mail.ru", async function(){
         await driver.quit();
     });
 
-    it("check email account entered correctly", async function(){
+    it("should login email account succesfully", async function(){
         driver = await new Builder().forBrowser("chrome").build();
 
         await driver.manage().setTimeouts( { implicit: 10000 } );
 
-        await driver.get("https://mail.ru");
+        await driver.get(url);
         await driver.manage().window().maximize();
         await driver.findElement(By.xpath("//button[@data-testid='enter-mail-primary']")).click();
 
@@ -29,13 +29,20 @@ describe("smoke test for mail.ru", async function(){
         await driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass, Key.ENTER);       
         
         await driver.switchTo().defaultContent();
+        console.log(await driver.getTitle())
+
+        await assert.equal(await driver.getTitle(), pageTitle)
+    
+    });
+
+    it("should check name of email account is correct", async function(){
 
         const accountHeader = await driver.findElement(By.css("span.ph-project__user-name.svelte-1hiqrvn"))
         .getText()
         .then((accountHeader) => { return accountHeader; });
 
         console.log(accountHeader);
-        await assert.strictEqual(accountHeader, "gracev.belbisnesresurs@mail.ru");
+        await assert.strictEqual(accountHeader, emaillName);
 
         
     });
@@ -43,20 +50,12 @@ describe("smoke test for mail.ru", async function(){
     it("check unread messages", async function(){
 
         await driver.findElement(By.css("a[href='/inbox/']")).click()
-        const incomming = driver.wait(function() {
-            return driver.findElements(WebDriver.By.css(".llc.llc_normal.llc_new.llc_new-selection.js-letter-list-item.js-tooltip-direction_letter-bottom"))
-            .then(function(elems) {
-                var length = elems.length; // Here's your length!
-                return length
-            });
-          }, 3000, 'Failed to find element after 3 seconds');
-        
-        await assert.notStrictEqual(true, incomming)
-        
-        console.log(incomming);
-       
-
+        const incomming = await driver.findElements(By.css(".llc.llc_normal.llc_new.llc_new-selection.js-letter-list-item.js-tooltip-direction_letter-bottom"))
+        for(let message of incomming) {
+            let text = message.getText()
+            return text
+        }
+        await expect(text).toInclude(headers)
     });
-
 });
 
---
+++ utils/constants.js
@@ -0,0 +1,7 @@
+const assertions = {
+    url: 'https://mail.ru',
+    emaillName: "gracev.belbisnesresurs@mail.ru",
+    pageTitle: 'Mail.ru: почта, поиск в интернете, новости, игры',
+    headers: 'Mail.ru'
+}
+module.exports = assertions
