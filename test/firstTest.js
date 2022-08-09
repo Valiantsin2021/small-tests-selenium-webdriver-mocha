a9646c5dacb9b238ec800435b856cdb607a22c6c
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2022-08-09 13:24:08 +0200

    add waits for both tests

--- test/firstTest.js
+++ test/firstTest.js
@@ -1,61 +1,48 @@
-const {Builder, By, Key, WebDriver} = require("selenium-webdriver");
+const {Builder, By, Key, WebDriver, until} = require("selenium-webdriver");
 require("chromedriver");
 let assert = require("assert");
 let expect = require('expect')
 const { user, pass } = require('../utils/credentials.js')
 const { urlMail, emaillName, pageTitle, headers} = require('../utils/constants.js')
-
 describe("Smoke test for mail.ru", async function(){
-    
     after(async function(){
         await driver.quit();
     });
-
     it("Should login email account succesfully", async function(){
         driver = await new Builder().forBrowser("chrome").build();
-
         await driver.manage().setTimeouts( { implicit: 10000 } );
-
         await driver.get(urlMail);
         await driver.manage().window().maximize();
-        await driver.findElement(By.xpath("//button[@data-testid='enter-mail-primary']")).click();
-
+        const email = await driver.wait(until.elementLocated(By.xpath("//button[@data-testid='enter-mail-primary']")), 2000)
+        await email.click();
         const iframe = await driver.findElement(By.css('div.ag-popup__frame__layout.ag-popup__frame__layout-desktop > iframe'))
         await driver.switchTo().frame(iframe);
-
-
-        await driver.findElement(By.xpath("//input[@name='username']")).sendKeys(user, Key.ENTER);
-        
-        await driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass, Key.ENTER);       
-        
+        const emailUser = await driver.wait(until.elementLocated(By.xpath("//input[@name='username']")), 3000)
+        await emailUser.sendKeys(user, Key.ENTER);
+        const emailPass = await driver.wait(until.elementLocated(By.xpath("//input[@name='password']")), 3000)
+        await emailPass.sendKeys(pass, Key.ENTER);       
         await driver.switchTo().defaultContent();
         console.log(await driver.getTitle())
-
         await assert.equal(await driver.getTitle(), pageTitle)
     
     });
-
     it("Should check name of email account is correct", async function(){
-
-        const accountHeader = await driver.findElement(By.css("span.ph-project__user-name.svelte-1hiqrvn"))
+        const accountHeader = await driver.wait(until.elementLocated(By.css("span.ph-project__user-name.svelte-1hiqrvn")), 3000)
         .getText()
         .then((accountHeader) => { return accountHeader; });
-
         console.log(accountHeader);
         await assert.strictEqual(accountHeader, emaillName);
-
-        
     });
-
     it("Should check unread messages", async function(){
-
-        await driver.findElement(By.css("a[href='/inbox/']")).click()
-        const incomming = await driver.findElements(By.css(".llc.llc_normal.llc_new.llc_new-selection.js-letter-list-item.js-tooltip-direction_letter-bottom"))
+        const popup = await driver.wait(until.elementLocated(By.css("a[class='ph-project svelte-jq5qv5 ph-project_current']")),2000)
+        await popup.click()
+        const inbox = await driver.wait(until.elementLocated(By.css("a[href='/inbox/']")), 3000)
+        await inbox.click()
+        const incomming = await driver.wait(until.elementsLocated(By.css(".llc.llc_normal.llc_new.llc_new-selection.js-letter-list-item.js-tooltip-direction_letter-bottom")), 3000)
         for(let message of incomming) {
-            let text = message.getText()
+            let text = await message.getText()
             return text
         }
         await expect(text).toInclude(headers)
     });
 });
-
--- test/secondTest.js
+++ test/secondTest.js
@@ -3,40 +3,33 @@ require("chromedriver");
 let should = require("chai").should();
 let assert = require("assert");
 const {urlAmazon, titleAmazon, searchAmazon, searchTitle, price} = require('../utils/constants.js')
-
 describe("Should succesfully search Amazon.es with specified keywords", async function(){
-    
     after(async function(){
         await driver.quit();
     });
-
     it("Should successfully open Amazon.es page", async function(){
-
         driver = await new Builder().forBrowser("chrome").build();
         await driver.manage().setTimeouts( { implicit: 10000 } );
         await driver.manage().window().maximize();
         await driver.get(urlAmazon);
         await assert.equal(await driver.getTitle(), titleAmazon)
     });
-
     it("Should successfully search Amazon.es page 'Roomba'", async function(){
         let cookies = await driver.wait(until.elementLocated(By.css('#sp-cc-accept')), 2000)
-        await cookies.click();
+        if(cookies) {
+            await cookies.click();
+        }
         let search = await driver.wait(until.elementLocated(By.css('#twotabsearchtextbox')), 2000)
         await search.sendKeys(searchAmazon, Key.ENTER)
         const results = await driver.wait(until.elementLocated(By.xpath("//span[text()='RESULTADOS']")), 2000)
         await assert.equal(await results.getText(), searchTitle)
-
     });
-
-    it("Should check price of Roomba i7556", async function(){
-            
+    it("Should check price of Roomba i7556", async function(){     
         let roomba = await driver.wait(until.elementLocated(By.xpath('//span[text()="Robot Aspirador iRobot Roomba i3+ - Autovaciado de Suciedad - Ideal para Mascotas - Sugerencias Personalizadas - Compatible con tu Asistente de Voz - Color Gris Azulado"]')), 2000)
         await roomba.click()
         let priceReceived = await driver.wait(until.elementLocated(By.css('#corePriceDisplay_desktop_feature_div  span.a-price-whole')), 2000)
         await assert.strictEqual(await priceReceived.getText(), price)
     });
-
     it("Should check image of Roomba i7556 is present", async function(){
         let image = await driver.wait(until.elementLocated(By.css('#imgTagWrapperId')), 2000)
         await assert.ok(true, image)
